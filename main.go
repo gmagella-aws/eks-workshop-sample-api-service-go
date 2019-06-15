8a3a64fcb37fdf39580cc58ad15f3888bd8a3781
Geraldo Magella <44304595+gmagella-ca@users.noreply.github.com>
2019-06-15 16:31:03 -0400

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World - New Geraldo Version"}
+		res := &response{Message: "Hello World - New Geraldo Version, after removal"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
